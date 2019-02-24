168fb63e92f535ab76b663d01ccb63d3a1b9b591
Cheri Haggard <gonzocher@cox.net>
2019-02-23 16:15:41 -0800

    In finished state

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.media.Image;
+
 /**
  * {@Link Word} represents a vocabulary word that the user wants to learn.
  * It contains a default translation and a Miwok translation for that word.
@@ -15,12 +17,40 @@ public class Word {
      */
     private String mMiwokTranslation;
 
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+
     /**
-     * Create a new Word object.
+     * Image for the word
      */
-    public Word(String defaultTranslation, String miwokTranslation) {
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * Create a new Word object with three inputs.
+     * @param defaultTranslation is the English word.
+     * @param miwokTranslation is the Miwok word.
+     * @param audioResourceId is the resource ID for the audio file associated with this word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Create a new Word object with three inputs.
+     * @param defaultTranslation is the English word.
+     * @param miwokTranslation is the Miwok word.
+     * @param imageResourceId is the drawable resource ID for the word.
+     * @param audioResourceId is the resource ID for the audio file associated with this word
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     /**
@@ -35,5 +65,26 @@ public class Word {
      */
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    /**
+     * Get the image for the word.
+     */
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Check if the Word has an image.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,10 +3,12 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -14,12 +16,21 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+    /**Resource ID for the background color for this list of words*/
+    private int mColorResourceId;
 
-public WordAdapter(Activity context, ArrayList<Word> words) {
+    /**
+     * Create a new {@link WordAdapter} object.
+     *
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param words is the list of {@link Word}s to be displayed.
+     * @param colorResourceId is the resource ID for the background color for this list of words
+     */
+
+public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
     super(context,0,words);
+    mColorResourceId = colorResourceId;
 }
-
-
 
     @NonNull
     @Override
@@ -37,6 +48,25 @@ public WordAdapter(Activity context, ArrayList<Word> words) {
         TextView miwokTranslationView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTranslationView.setText(currentWord.getMiwokTranslation());
 
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if (currentWord.hasImage() ) {
+            //Set the image to the resource
+            imageView.setImageResource(currentWord.getmImageResourceId());
+            //Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else {
+            //Otherwise, hide the image view
+            imageView.setVisibility(View.GONE);
+        }
+
+        //Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        //Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        //Set the background color of the text container view
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
